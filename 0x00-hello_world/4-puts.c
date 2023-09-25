4beba5704767491cb614fc3437e8fe17ac4c1828
medalimelb <melbousmedali10@gmail.com>
2023-09-25 13:58:41 -0700

    Initial com

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,20 @@
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
 #include <stdio.h>
+
 /**
-* main - Entry point 
-*Return: Always 0 (Success)
-*/
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");	
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
+
